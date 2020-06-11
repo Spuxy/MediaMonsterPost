c23fbf9f946e9758ff7bed84a29cf00591bf9729
Spuxy <Spuxy@seznam.cz>
2020-06-11 20:53:30 +0200

    uprava funkce

--- app/PostOffice.php
+++ app/PostOffice.php
@@ -4,18 +4,23 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class PostOffice extends Model
-{
+class PostOffice extends Model {
+
 	protected $guarded = [];
 
 	public function hours() {
 		return $this->hasMany(PostOfficeHour::class);
-    }
+	}
 
-    public function isAlreadySaved($address){
-		if (count(PostOffice::where('Address', $address)->get())==0){
+	public function isAlreadySaved($address) {
+		if ( $this->getAddress($address) == 0 ) {
 			return false;
 		}
+
 		return true;
-    }
+	}
+
+	public function getAddress($address) {
+		return count(PostOffice::where('Address', $address)->get());
+	}
 }
